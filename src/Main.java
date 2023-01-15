937b622addcb3ba0816a6c8c1afca3fab2ed43e9
Avraham Gidon <workshandesai19@gmail.com>
2023-01-15 16:24:35 +0200

    Change print line

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Avraham = king");
         System.out.println(" i believe i can fly");
+        System.out.println(" i believe i can fly");
 
     }
 }
